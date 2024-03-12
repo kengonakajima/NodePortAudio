460e35d85998e738e3f8321f69878832ddce6ee0
kengo nakajima <kengo.nakajima@gmail.com>
2024-03-12 15:43:06 +0900

    rm

--- recplay.js
+++ recplay.js
@@ -7,7 +7,6 @@ PortAudio.listDevices();
 PortAudio.startMic();
 PortAudio.startSpeaker();
 
-// "******      " のような文字列を返す
 function getVolumeBar(l16sample) {
   const vol=Math.abs(l16sample);
   const bar = vol / 1024;
@@ -15,16 +14,14 @@ function getVolumeBar(l16sample) {
   return "*".repeat(bar)+" ".repeat(space); 
 }
 
-let g_maxSample=0;
 setInterval(()=>{
   const samples=PortAudio.getRecordedSamples();
-  // 最大音量を記録
-  g_maxSample=0;
+  let maxSample=0;
   for(let i=0;i<samples.length;i++) {
     const sample=samples[i];    
-    if(sample>g_maxSample) g_maxSample=sample; 
+    if(sample>maxSample) maxSample=sample; 
   }
-  console.log("volume:",getVolumeBar(g_maxSample));
+  console.log("volume:",getVolumeBar(maxSample));
   PortAudio.pushSamplesForPlay(samples);
   PortAudio.discardRecordedSamples(samples.length);  
 },25);
